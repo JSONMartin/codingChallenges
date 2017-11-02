0ea0496fcea2db067b9cf138e83bbf0d1e3660a0
JSONMartin <jmartin@popsugar.com>
2017-11-01 20:28:01 -0700

    Updates for 2017-11-01
    Updates for 2017-11-01
    
    Updates for 2017-11-01

--- uploadRepoToGithub.py
+++ uploadRepoToGithub.py
@@ -3,12 +3,12 @@ import subprocess, logging, sys, datetime
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s', filename="githubPush.log")
 logging.info('-----------------------------------------')
-logging.debug('Program starts')
+logging.info('Attempting to sync github repo...')
 
 # Check for untracked files
 stdout = subprocess.getoutput('git status')
 if 'nothing to commit' in stdout:
-    logging.info("Git clean, no changes made")
+    logging.info("Repo folder clean, no changes made")
     sys.exit()
 
 # Add commit
@@ -19,4 +19,5 @@ stdout = subprocess.getoutput(f'git commit -m "Updates for {datetime.date.today(
 
 # Push to Github
 stdout = subprocess.getoutput('git push')
-logging.info(stdout)
+
+logging.info(f"Github push results:\n{stdout}")
