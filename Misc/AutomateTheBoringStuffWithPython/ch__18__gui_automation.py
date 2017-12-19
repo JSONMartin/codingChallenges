e4da4843d2aed273f0c922f71304649c68eaceb2
System Administrator <root@MBPR-jmartin-SF.home>
2017-12-18 20:30:01 -0800

    Updates for 2017-12-18

--- Misc/AutomateTheBoringStuffWithPython/ch__18__gui_automation.py
+++ Misc/AutomateTheBoringStuffWithPython/ch__18__gui_automation.py
@@ -1,4 +1,4 @@
-import pyautogui
+import pyautogui, time, os
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = 1
@@ -6,6 +6,7 @@ pyautogui.FAILSAFE = 1
 width, height = resolution = pyautogui.size()
 
 print(f"Width:{width}, Height:{height}")
+
 
 def currentMousePosition():
     return pyautogui.position()
@@ -26,5 +27,53 @@ def moveInSquareRelative():
         pyautogui.moveRel(-100, 0, duration=0.25)
         pyautogui.moveRel(0, -100, duration=0.25)
 
-print(f"Current Mouse Position:{currentMousePosition()}")
-moveInSquareRelative()
+# print(f"Current Mouse Position:{currentMousePosition()}")
+# moveInSquareRelative()
+
+def clickLocation():
+    clickLocation = (1400, 800)
+    print("Clicking at:", clickLocation)
+    pyautogui.click(clickLocation[0], clickLocation[1], button='right')
+
+
+def drawSpiral():
+    DUR = .2
+    time.sleep(5)
+    pyautogui.click()
+    distance = 200
+
+    while distance > 0:
+        pyautogui.dragRel(distance, 0, duration=DUR)
+        distance -= 5
+        pyautogui.dragRel(0, distance, duration=DUR)
+        pyautogui.dragRel(-distance, 0, duration=DUR)
+        distance -= 5
+        pyautogui.dragRel(0, -distance, duration=DUR)
+
+
+# drawSpiral()
+
+def scrollTest():
+    pyautogui.click()
+    pyautogui.scroll(200)
+
+
+# scrollTest()
+
+
+def imageRecognition():
+    print('hi')
+    print(os.listdir())
+    RECOGNITION_FILE = os.getcwd() + '/imageRecognition.png'
+    print(RECOGNITION_FILE)
+    pyautogui.locateOnScreen(RECOGNITION_FILE)
+
+# imageRecognition()
+
+
+def typeTest():
+    DELAY = .25
+    pyautogui.click(1400, 300)
+    pyautogui.typewrite('Hello World', DELAY)
+
+typeTest()
