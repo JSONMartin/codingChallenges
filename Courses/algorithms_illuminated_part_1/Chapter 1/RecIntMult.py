3cf5b4d9f6a235b65b0fdb10e8c35aa5b5d23ef7
JSONMartin <jmartin@popsugar.com>
2017-12-21 23:11:34 -0800

    Updates for 2017-12-21

---
+++ Courses/algorithms_illuminated_part_1/Chapter 1/RecIntMult.py
@@ -0,0 +1,31 @@
+"""
+Input: two n-digit positive integers x and y
+Output: the product x · y
+Assumption: n is a power of 2.
+
+Multiplies by breaking up number into 2 half, a/b for x and c/d for y respectively.
+
+n = length of digits
+"""
+
+
+def recIntMult(x, y):
+    xDigits = str(x)
+    yDigits = str(y)
+    n = len(xDigits)
+
+    if n == 1:  # base case
+        return x * y
+
+    a, b = int(xDigits[:n // 2]), int(xDigits[n // 2:])
+    c, d = int(yDigits[:n // 2]), int(yDigits[n // 2:])
+    print(f"a:{a}, b:{b}, c:{c}, d:{d}")
+
+    result = 10**n * recIntMult(a, c) + 10 ** (n / 2) * (recIntMult(a, d) + recIntMult(b, c)) + recIntMult(b, d)
+    return int(result)
+
+
+X, Y = 12345678, 87654321
+# X, Y = 1234, 5678
+result = recIntMult(X, Y)
+print(result)
