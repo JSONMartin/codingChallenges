1102b3aae176870fc5ff7d5e2e8419153af86614
Jason Martin <jmartin@popsugar.com>
2018-02-20 20:58:04 -0800

    Newest Codefights levels

--- CodeFights/arcade/level2-almostIncreasingSequence.js
+++ CodeFights/arcade/level2-almostIncreasingSequence.js
@@ -1,26 +1,21 @@
-// function almostIncreasingSequence(sequence, remaining = 1, maximum) {
-//     if (remaining < 0) return false
-//     else if (sequence.length === 1) return true
-
-//     if (sequence[0] > sequence[1]) return almostIncreasingSequence(sequence.slice(1), remaining - 1, maximum)
-
-//     maximum = sequence[0]
-//     return sequence[1] > maximum && almostIncreasingSequence(sequence.slice(1), remaining, maximum)
-// }
-
-// // almostIncreasingSequence([1, 3, 2])
-// let res = almostIncreasingSequence([1, 2, 1, 2])
-// console.log(res);
-
-const almostIncreasingSequence = (sequence) => {
-    let skips = 1;
+const almostIncreasingSequence = (sequence, remaining = 1) => {
+    if (remaining < 0) return false
 
     for (let i = 0; i < sequence.length - 1; i++) {
-        if (skips < 0) return false
-        if (sequence[i] > sequence[i + 1]) {
-            skips -= 1
+        if (sequence[i] >= sequence[i + 1]) {
+            let removeFirstNumber = almostIncreasingSequence([...sequence.slice(0, i), ...sequence.slice(i + 1)], remaining - 1)
+            let removeSecondNumber = almostIncreasingSequence([...sequence.slice(0, i + 1), ...sequence.slice(i + 2)], remaining - 1)
+            return removeFirstNumber || removeSecondNumber
         }
     }
 
-    return true;
+    return true
 }
+
+// TESTS
+
+let res = almostIncreasingSequence([1, 2, 3, 4, 3, 6])
+// let res = almostIncreasingSequence([1, 3, 2])
+// let res = almostIncreasingSequence([1, 3, 2, 1])
+// let res = almostIncreasingSequence([1, 2, 1, 2])
+console.log(res);
